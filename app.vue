899b6c6515139fde3ca218156bcb4e37f5566b24
GYNFLO <gynflo@gmail.com>
2023-01-16 21:53:57 +0100

    Starting & Finish design left sidebar

--- app.vue
+++ app.vue
@@ -1,5 +1,27 @@
+<script setup>
+const darkMode = ref(false);
+</script>
+
 <template>
-  <div>
-    <h1 class="text-red-500">Twitter Clone</h1>
+  <div :class="{ dark: darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+        >
+          <!-- Left Sidebar -->
+          <div class="hidden md:block xs:col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+          <!-- Main Sidebar -->
+          <main></main>
+          <!-- Right Sidebar -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped></style>
